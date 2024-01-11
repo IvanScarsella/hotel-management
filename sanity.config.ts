c3577426acfccbf4965f43429fcfdd3d9b622b4a
Iván Scarsella <scarsellaivan@gmail.com>
2024-01-11 16:30:50 -0300

    add sanity configuration

---
+++ sanity.config.ts
@@ -0,0 +1,26 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemas'
+// import {getStartedPlugin} from '../cms/plugins/sanity-plugin-tutorial'
+
+// const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'hotel-management',
+
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+
+  basePath: '/studio',
+
+  plugins: [structureTool(), visionTool(),
+    //  ...(isDev ? devOnlyPlugins : [])
+    ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
