a4e19e8eb4d79ac6684421155543ab482ffe420d
Iván Scarsella <scarsellaivan@gmail.com>
2024-01-16 15:40:01 -0300

    add add Hero Section

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,8 +1,13 @@
 import HeroSection from "@/components/HeroSection/page"
+import PageSearch from "@/components/PageSearch/PageSearch"
 
 const Home = () => {
   return <>
     <HeroSection />
+    <PageSearch />
+    {/* FEATURED ROOM */}
+    {/* GALLERY */}
+    {/* NEWS LETTER */}
   </>
 }
 
